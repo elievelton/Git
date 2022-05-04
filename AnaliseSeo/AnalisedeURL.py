43f11661354a9a9b6553e503334d9d41c4287b77
elievelton <elievelton@gmail.com>
2022-05-04 10:06:53 -0300

    Pegando mais partes para tratamento

--- AnaliseSeo/AnalisedeURL.py
+++ AnaliseSeo/AnalisedeURL.py
@@ -16,15 +16,33 @@ except URLError:
 else:
     res = BeautifulSoup(html.read(),"html5lib")
     
-    titulo = res.findAll("h3", {"class": "post-title"})# pegando o título
+    titulo = res.findAll("h3", {"class": "post-title"})# pegando o título do texto
+    titulo_da_pagina =res.findAll("title")
     texto = res.findAll("div", {"class": "post-body"}) # pegando o corpo do texto
+    link = res.select("body a" ) #pega os titulos dos links
+    h2 = res.findAll("h2")# pega tudo que tiver em H2 que são os topicos do texto
+    head = res.findAll("description") #pega o cabeçario de um site
+
 
 
     #mostrando o que tem dentro da variavel 
+    print("------------------------------------Titulo----------------------------------------------")
     for tag in titulo:
         print(tag.getText())
-    
+    print("------------------------------------Corpo do texto----------------------------------------------")
     for tag in texto:
         print(tag.getText())
+    print("------------------------------------Titulo dos links----------------------------------------------")
+    for tag in link:
+        print(tag)# mostra o link
+        print(tag.getText()) #mostra o titulo
+    print("------------------------------------Tag H2----------------------------------------------")
+    for tag in h2:
+        print(tag.getText())
+    print("------------------------------------Tag Head----------------------------------------------")
+    for tag in head:
+        print(tag.getText())
+    print("------------------------------------Titulo da pagina----------------------------------------------")
+    print(titulo_da_pagina)
 
  
