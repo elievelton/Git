b122ed264b1865661ab5bdc8f424a84fd2354505
elievelton <elievelton@gmail.com>
2022-05-04 13:36:22 -0300

    feat: capturando a descrição

--- AnaliseSeo/AnalisedeURL.py
+++ AnaliseSeo/AnalisedeURL.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 
 
 try:
-    
-    url = 'https://www.cataimagem.com/2019/03/moderninha-pro-ou-sumup-total-qual-devo.html'
+    #https://www.mundoblogger.com.br/2021/05/como-desenvolver-softwares-na-area-da-saude.html  url de testes
+    #https://www.cataimagem.com/2019/03/moderninha-pro-ou-sumup-total-qual-devo.html  url de testes
+    url = 'https://www.mundoblogger.com.br/2021/05/como-desenvolver-softwares-na-area-da-saude.html'
     html = urlopen(url)
     
     
@@ -21,9 +22,11 @@ else:
     titulo = res.findAll("h3", {"class": "post-title"})# pegando o título do texto body
     titulo_da_pagina =res.findAll("title") #pega o titulo da pagina
     texto = res.findAll("div", {"class": "post-body"}) # pegando o corpo do texto
+    texto2 = res.findAll("p")
     link = res.select("body a" ) #pega os titulos dos links
     h2 = res.findAll("h2")# pega tudo que tiver em H2 que são os topicos do texto
     img = res.select("img") #pega o codigo de uma imagem ou varias imagens. Precisa tratar pegando tudo que tive <img alt"aqui fica o titulo da imagem
+    description = res.findAll("meta", {"property": "og:description"}) # pegando a descrição da pagina
     
     
     #mostrando o que tem dentro da variavel 
@@ -45,5 +48,9 @@ else:
         print(tag)
     print("------------------------------------Titulo da pagina----------------------------------------------")
     print(titulo_da_pagina)
-
+    print("------------------------------------Texto2 alguns site só pegam o texto por aqui----------------------------------------------")
+    for tag in texto2:
+        print(tag.getText())
+    print("------------------------------------descricao----------------------------------------------")
+    print(description)
  
